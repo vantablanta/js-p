737de0b38a2bfc3bd7a99940e79913057fa9300e
vanta blanta <vantablanta@gmail.com>
2022-04-14 18:11:26 +0300

    d1-data types

--- datatype.js
+++ datatype.js
@@ -1,1 +1,32 @@
-console.log (typeof name, typeof number, typeof dead, typeof height, money)
+//5.
+console.log(typeof name, typeof number, typeof dead, typeof height, money)
+
+
+//6. 
+let time
+let age
+let job
+let car
+
+//7.
+let itsRaining = true
+let bestTm = "Julius"
+let plants = 4
+let win = null
+
+//8. 
+var firstName = "Michelle"
+var lastName = "Njeri"
+var maritalStatus = false
+var country = "Kenya"
+var years = 22
+
+//9.
+var firstName = "Michelle"; lastName = "Njeri"; maritalStatus = false; country = "Kenya", years = "22"
+
+//10.
+let myAge = 22
+let yourAge = 33
+
+console.log(`My age is ${myAge}`)
+console.log(`Your age is ${yourAge}`)
